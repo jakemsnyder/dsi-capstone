7114d4274e4e463370a0d614e8892af8064dd955
Jake Snyder <jakemsnyder2@gmail.com>
2018-11-25 13:37:22 -0500

    include all generic demographics

--- MRP.R
+++ MRP.R
@@ -1,79 +1,50 @@
 library(tidyverse)
 library(nnet)
 
-# this.packages <- c("data.table", "fitdistrplus", 
-#                    "chron", "R.utils", "descr", "xtable", "car", 
-#                    "foreach", "coda", "parallel",
-#                    "doParallel", "arm", 'dplyr',
-#                    "caret", "glmnet", 'nnet',
-#                    "e1071", 'stringr');
-# demographic_combinations <- c(list('Education', 'Gender', 'Age'),
-#                              combn(c('Education', 'Gender', 'Age'), 2, simplify = F),'Census_Division')
-# party, gender, age, race, education, urbanicity, married
-
-demographic_combinations <- list(c('gender', 'age', 'race'),
-                                 c('gender', 'race'),
-                                 c('race', 'age'),
-                                 c('gender', 'age'),
-                                 'gender', 'age', 'race')
-# state_map <- setNames(state.abb, state.name)
-
-data <- read_csv('Data/all_generic.csv') %>%
-  select(gender, age, race, vote2018) %>%
+demographics <- c('party', 'gender', 'age', 'race', 'education', 'married', 'urbanicity')
+demographic_combinations <- c(combn(demographics, simplify=FALSE, 1),
+                              combn(demographics, simplify=FALSE, 2),
+                              combn(demographics, simplify=FALSE, 3),
+                              combn(demographics, simplify=FALSE, 4),
+                              combn(demographics, simplify=FALSE, 5),
+                              combn(demographics, simplify=FALSE, 6),
+                              combn(demographics, simplify=FALSE, 7))
+# load('Data/all_generic.RData')
+data <- read_csv('Data/all_generic.csv',
+                 col_types=cols(party = col_factor(levels=c('Dem', 'Rep', 'Ind')),
+                                gender = col_factor(levels=c('Male', 'Female')),
+                                age = col_factor(levels=c('18 - 24', '25 - 34', '35 - 44', '45 - 54', '> 54')),
+                                race = col_factor(levels=c('White', 'Hispanic', 'Black', 'Other')),
+                                education = col_factor(levels=c('No Bachelors', 'Bachelors')),
+                                urbanicity = col_factor(levels=c('R1', 'R2', 'S3', 'S4', 'U5', 'U6')),
+                                vote2018 = col_character(),
+                                married = col_factor(levels=c('Unmarried', 'Married')),
+                                wave = col_skip())) %>%
   mutate(vote2018 = case_when(vote2018 == 'Democratic candidate' ~ 'Democrat',
-                              vote2018 == 'Republican candidate' ~ 'Republican'),
-         race = ifelse(race=='Other Race', 'Other', race)) %>%
+                              vote2018 == 'Republican candidate' ~ 'Republican')) %>%
   filter(!is.na(vote2018)) %>%
-  mutate(gender = factor(gender, levels=c('Male', 'Female')),
-         age = factor(age, levels=c('18 - 24', '25 - 34', '35 - 44', '45 - 54', '> 54')),
-         race = factor(race, levels=c('White', 'Hispanic', 'Black', 'Other')),
-         vote2018 = factor(vote2018, levels=c('Democrat', 'Republican')))
+  mutate(vote2018 = factor(vote2018, levels=c('Democrat', 'Republican')))
 
-pops <- read_csv('Data/population_data.csv') %>%
-  mutate(race = factor(race, levels=levels(data$race)),
-         gender = factor(gender, levels=levels(data$gender)),
-         age = factor(age, levels=levels(data$age)))
+pops <- read_csv('Data/population_data.csv',
+                 col_types=cols(age = col_factor(levels=levels(data$age)),
+                                urbanicity = col_factor(levels=levels(data$urbanicity)),
+                                gender = col_factor(levels=levels(data$gender)),
+                                state = col_skip(),
+                                race = col_factor(levels=levels(data$race)),
+                                education = col_factor(levels=levels(data$education)),
+                                married = col_factor(levels=levels(data$married)),
+                                party = col_factor(levels=levels(data$party)),
+                                congressional_district = col_skip(),
+                                N = col_double()))
 
-
-# data <- read.csv('Data/generic_data.csv', stringsAsFactors = FALSE) %>%
-#   mutate(State_Abb = recode(State, !!!state_map),
-#          Vote_House = factor(Vote_House,
-#                              levels=c('Republican',
-#                                       'Democrat',
-#                                       'Other/Not sure',
-#                                       'Wont vote')),
-#          Trump_Sentiment = factor(Trump_Sentiment,
-#                                   levels=c('Approve Strongly',
-#                                            'Approve Weakly',
-#                                            'Neither Approve nor Disapprove',
-#                                            'Disapprove Weakly',
-#                                            'Disapprove Strongly')),
-#          Age = factor(Age,
-#                       levels=c('18 - 24',
-#                                '25 - 34',
-#                                '35 - 44',
-#                                '45 - 54',
-#                                '> 54')),
-#          Gender = factor(Gender,
-#                          levels=c('female',
-#                                   'male')),
-#          Education = factor(Education,
-#                             levels=c('middle_school',
-#                                      'high_school',
-#                                      'vocational_technical_college',
-#                                      'university',
-#                                      'postgraduate')),
-#          Census_Division = factor(Census_Division))
-
-pred_pops <- pops %>% group_by(gender, age, race) %>% summarise(N=sum(N))
-m <- multinom(vote2018 ~ (gender + age + race)^2, data, maxit=1000)
+pred_pops <- pops %>% group_by_if(is.factor) %>% summarise(N=sum(N))
+m <- multinom(vote2018 ~ (age + urbanicity + gender + race + education + married)^2, data, maxit=1000)
 pred <- predict(m, pred_pops, 'probs')
 
 pred_pops$pred_val = pred_pops$N * pred
 
 df <- list(Demographic_Type = 'National', Demographic = 'National',
            Vote_R = sum(pred_pops$pred_val)/sum(pred_pops$N)) %>% as.tibble()
-
 for(i in 1:length(demographic_combinations)){
   vars = demographic_combinations[[i]]
   temp = pred_pops %>% group_by_(.dots = lapply(vars, as.symbol)) %>%
