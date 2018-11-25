babd1619121af60b5343a2b6faa819cdbbd33b0a
Jake Snyder <jakemsnyder2@gmail.com>
2018-11-25 10:41:28 -0500

    add Data folder reference

--- generic_preprocessing.R
+++ generic_preprocessing.R
@@ -2,12 +2,10 @@
 # setwd('D:/Capstone_GitHub/data/')
 
 library(tidyverse)
-library(dplyr)
 library(readxl)
-load("generic_ballot.RData")
+load("Data/generic_ballot.RData")
 
 generic <- voting2018
-summary(generic)
 
 # lowercase batch 1 columns to match 2nd batch
 names(generic) <- tolower(names(generic))
@@ -26,18 +24,18 @@ loadRData <- function(fileName){
 }
 
 # load batch2
-survey1 <- loadRData("Pollfish_Survey_FI10002d.378826.moreifo_completed2.RData")
-survey2 <- loadRData("Pollfish_Survey_FI10006b.378910.moreifo_completed2.RData")
-survey3 <- loadRData("Pollfish_Survey_IN10001a.406052.moreifo_completed2.RData")
-survey4 <- loadRData("Pollfish_Survey_IN10004.324889.moreifo_completed2.RData")
-survey5 <- loadRData("Pollfish_Survey_IN10005.324886.moreifo_completed2.RData")
-survey6 <- loadRData("Pollfish_Survey_IN10006.324887.moreifo_completed2.RData")
-survey7 <- loadRData("Pollfish_Survey_IN10007b.378647.moreifo_completed2.RData")
-survey8 <- loadRData("Pollfish_Survey_SC10001a.338700.moreifo_completed2.RData")
-survey9 <- loadRData("Pollfish_Survey_SC10002a.338699.moreifo_completed2.RData")
-survey10 <- loadRData("Pollfish_Survey_SC10003b.375937.moreifo_completed2.RData")
-survey11 <- loadRData("Pollfish_Survey_SC10004c.375939.moreifo_completed2.RData")
-survey12 <- loadRData("Pollfish_Survey_SC10005a.338697.moreifo_completed2.RData")
+survey1 <- loadRData("Data/Pollfish_Survey_FI10002d.378826.moreifo_completed2.RData")
+survey2 <- loadRData("Data/Pollfish_Survey_FI10006b.378910.moreifo_completed2.RData")
+survey3 <- loadRData("Data/Pollfish_Survey_IN10001a.406052.moreifo_completed2.RData")
+survey4 <- loadRData("Data/Pollfish_Survey_IN10004.324889.moreifo_completed2.RData")
+survey5 <- loadRData("Data/Pollfish_Survey_IN10005.324886.moreifo_completed2.RData")
+survey6 <- loadRData("Data/Pollfish_Survey_IN10006.324887.moreifo_completed2.RData")
+survey7 <- loadRData("Data/Pollfish_Survey_IN10007b.378647.moreifo_completed2.RData")
+survey8 <- loadRData("Data/Pollfish_Survey_SC10001a.338700.moreifo_completed2.RData")
+survey9 <- loadRData("Data/Pollfish_Survey_SC10002a.338699.moreifo_completed2.RData")
+survey10 <- loadRData("Data/Pollfish_Survey_SC10003b.375937.moreifo_completed2.RData")
+survey11 <- loadRData("Data/Pollfish_Survey_SC10004c.375939.moreifo_completed2.RData")
+survey12 <- loadRData("Data/Pollfish_Survey_SC10005a.338697.moreifo_completed2.RData")
 
 # select columns for all data
 col_select <- function(x) { 
@@ -54,13 +52,6 @@ for (i in datasets){
   all_generic <- rbind(all_generic, i)
 }
 
-save(all_generic, file = "all_generic.RData")
+save(all_generic, file = "Data/all_generic.RData")
 
 #load("all_generic.RData")
-
-
-
-
-
-
-
